9fe942b7aff9281af6c1644ca9d1c7706b3566ae
alexis <alexis@solexis.ml>
2022-11-23 23:08:41 -0300

    solexis web

---
+++ test/Main.hx
@@ -0,0 +1,13 @@
+﻿package;
+
+import utest.Runner;
+import utest.ui.Report;
+
+class Main {
+	static function main() {
+		final runner = new Runner();
+		runner.addCases(test.tests);
+		Report.create(runner);
+		runner.run();
+	}
+}
